d365a34a7d71cde4b706c0d9a4cd652900fe3229
Andre <126825475+andrevolima@users.noreply.github.com>
2024-04-01 15:02:17 -0300

    :broom: Opcoes.java

--- ProjetoPoo/src/Opcoes.java
+++ ProjetoPoo/src/Opcoes.java
@@ -1,64 +1,3 @@
-/* import java.util.Locale;
-import java.util.Scanner;
-
-public class Opcoes {
-
-	public static void Piramide() {
-        Locale.setDefault(Locale.US);
-        Scanner sc = new Scanner(System.in);
-
-        System.out.println("Digite varios numeros: ");
-
-        String seq_num = sc.nextLine();
-        String[] num = seq_num.split(" ");
-
-        int n = num.length;
-        int vet[] = new int[n];
-        for (int i = 0; i < n; i++) {
-            vet[i] = Integer.parseInt(num[i]);
-        }
-
-        System.out.println("Digite a quantidade de linhas da sua piramide: ");
-        int tam = sc.nextInt();
-
-        int rep = 0;
-        for (int e = 0; e < tam; e++) {
-            for (int f = 0; f <= e; f++) {
-                System.out.print(vet[rep % n] + " ");
-                rep++;
-            }
-            System.out.println();
-        }
-
-        int zeze = 0;
-        int[][] piramide = new int[tam][]; 
-        for (int c = 0; c < tam; c++) {
-            int[] linha = new int[c + 1]; 
-            for (int d = 0; d <= c; d++) {
-                linha[d] = vet[zeze % n];
-                zeze++;
-            }
-            piramide[c] = linha; 
-        }
-
-        int somaMenores = 0; 
-        for (int i = 0; i < piramide.length; i++) {
-            int menor = piramide[i][0]; 
-            for (int j = 1; j < piramide[i].length; j++) {
-                if (piramide[i][j] < menor) {
-                    menor = piramide[i][j]; 
-                }
-            }
-            somaMenores += menor; 
-        }
-
-        System.out.println("Soma dos menores numeros de cada linha: " + somaMenores);
-
-        sc.close();
-    }
-
-}   */
-
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -93,7 +32,7 @@ public class Opcoes {
 	            posicao++;
 
 	            if (f == 0 || numeroAtual < menorNumeroLinha) {
-	                menorNumeroLinha = numeroAtual; // Atualiza o menor número da linha
+	                menorNumeroLinha = numeroAtual; 
 	            }
 	        }
 
